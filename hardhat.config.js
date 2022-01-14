fb734a4f5ff81f1913c7d849a9ef15ce363073e3
Mir Imad Ahmed <mirimadahmed@outlook.com>
2022-01-14 20:59:56 +0500

    init

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    fuji: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43113,
+      accounts: ["50a70dd07254a31c06a8e765f6c4b8bb2072a86a5ab9ec0028c65b917efcfee9"],
+    },
+  },
+};
