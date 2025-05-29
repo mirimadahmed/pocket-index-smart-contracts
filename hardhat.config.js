af6173c9ea93448f213f748fbde172c367051332
Mir Imad Ahmed <mirimadahmed@outlook.com>
2025-05-29 15:11:30 +0800

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,85 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomicfoundation/hardhat-verify");
 
 module.exports = {
-  solidity: "0.8.0",
   networks: {
-    fuji: {
-      url: 'https://api.avax-test.network/ext/bc/C/rpc',
-      gasPrice: "auto",
-      chainId: 43113,
-      accounts: ["50a70dd07254a31c06a8e765f6c4b8bb2072a86a5ab9ec0028c65b917efcfee9"],
-    },
+    "mode": {
+      url: "https://sepolia.mode.network/",
+      chainId: 919,
+      accounts: ["5d7180f7a0c57b29688d9030a9eca8d06babb04bb542ac2e6b0be7a61f6dbc66"] //BE VERY CAREFUL, DO NOT PUSH THIS TO GITHUB
+    }
   },
+  etherscan: {
+    apiKey: {
+      "mode": "MODE-NETWORK-TESTNET",
+    },
+    customChains: [
+      {
+        network: "mode",
+        chainId: 919,
+        urls: {
+          apiURL: "https://sepolia.explorer.mode.network/api",
+          browserURL: "https://sepolia.explorer.mode.network",
+        },
+      },
+    ],
+  },
+  sourcify: {
+    enabled: false,
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.20",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          },
+        },
+      },
+      {
+        version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          },
+        },
+      },
+      {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          },
+        },
+      },
+      {
+        version: "0.5.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          },
+        },
+      },
+      {
+        version: "0.4.18",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          },
+        },
+      },
+    ],
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  }
 };
